ae5ae05420a507536744d6ce71b60d91feac253a
Arif Babu <babuarif5566@gmail.com>
2024-11-07 23:10:19 +0600

    Add files via upload
    Uploading the Project File

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const searchButton = document.querySelector('.search-button');
+//     const inputField = document.querySelector('.search-section input');
+  
+//     searchButton.addEventListener('click', () => {
+//       alert(`Searching for: ${inputField.value}`);
+//     });
+//   });
+  
